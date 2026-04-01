6a027cbc1568de6373fd727d14a4e4d733526267
angkasa <ariyowngsff@gmail.com>
2026-04-01 22:03:39 +0700

    Refactor Firebase configuration and imports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,44 +9,29 @@ const firebaseConfig = {
   measurementId: "G-DP212LNDK8"
 };
 
-// Import Firebase
+// Initialize Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-  getAuth, 
-  RecaptchaVerifier, 
-  signInWithPhoneNumber,
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword,
-  onAuthStateChanged, 
-  signOut, 
-  sendEmailVerification,
-  GoogleAuthProvider, 
-  FacebookAuthProvider, // <--- Tambahan Facebook
-  OAuthProvider,        // <--- Tambahan Apple
-  signInWithPopup    
+  getAuth, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider, signInWithPopup,
+  createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, sendEmailVerification 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, query, where, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Inisialisasi
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-// Inisialisasi Provider
+// Providers
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
-const appleProvider = new OAuthProvider('apple.com'); // Provider Apple
+const appleProvider = new OAuthProvider('apple.com');
 
 export { 
-  app, auth, db, storage, 
-  googleProvider, facebookProvider, appleProvider, // Export semua provider
-  RecaptchaVerifier, signInWithPhoneNumber,
-  createUserWithEmailAndPassword, signInWithEmailAndPassword,
+  app, auth, db, storage, googleProvider, facebookProvider, appleProvider,
+  signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword,
   onAuthStateChanged, signOut, sendEmailVerification,
-  signInWithPopup,
   doc, setDoc, getDoc, updateDoc, collection, addDoc,
   query, where, getDocs, serverTimestamp, ref, getDownloadURL
 };
